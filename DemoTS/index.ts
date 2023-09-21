f60c8ada0dbc13ab738da01557ef7b5cdf9a3e92
Anjali Krishna <71517113+anjalikrishna15@users.noreply.github.com>
2023-09-21 17:59:22 +0530

    Update index.ts

--- DemoTS/index.ts
+++ DemoTS/index.ts
@@ -6,15 +6,19 @@ import { Rocket } from "./rocket.js";
 var cargoWeight=parseInt(prompt("enter cargo weight"));
 var carName=prompt("enter cargo Name")
 
-
+//getting astronaut values
 var astronautWeight=parseInt(prompt("enter astronaut weight"));
 var astName=prompt("enter astronaut Name")
+
+//creating astronaut and cargo objects
 let car =new Cargo(cargoWeight,carName);
 let ast=new Astronaut(astronautWeight,astName);
 
+//getting rocket values
 var rocetkWeight=parseInt(prompt("enter rocket weight"));
 var rocketName=prompt("enter rocket Name")
 
+//creating rocket object
 let rock =new Rocket(rocketName,rocetkWeight);
 rock.addCargo(car);
 rock.addAstronaut(ast);
